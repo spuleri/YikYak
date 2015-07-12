b43cd94c24e6c4f4b2361f42236ec3a6c1325a90
Sergio Puleri <spuleri@ufl.edu>
2015-07-12 18:22:14 -0400

    Initial Commit

---
+++ YikYak/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YikYak
+//
+//  Created by Sergio Puleri on 7/12/15.
+//  Copyright (c) 2015 Sergio Puleri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YikYak/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YikYak
+//
+//  Created by Sergio Puleri on 7/12/15.
+//  Copyright (c) 2015 Sergio Puleri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
